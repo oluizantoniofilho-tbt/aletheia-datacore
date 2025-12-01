415722b8f3fc2085d632d6625c086882db9ced15
oluizantoniofilho <oluizantoniofilho@gmail.com>
2025-12-01 02:59:24 +0000

    ajuste no codigo mainpy

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 
+import os
+import sys
+from pathlib import Path
+
+# Adicionar a raiz do projeto ao PYTHONPATH
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT_DIR))
+
 from src.etl.receita_etl import ReceitaPipeline
 from src.etl.despesa_etl import DespesaPipeline
 
@@ -6,11 +14,16 @@ def run_pipelines():
     municipio = "taubate"
     ano = 2025
 
-    print("Executando ReceitaPipeline...")
+    print("\n=== EXECUTANDO PIPELINES DO ALETHEIA DATACORE ===\n")
+
+    print("➡️ ReceitaPipeline...")
     ReceitaPipeline().run(municipio, ano)
 
-    print("Executando DespesaPipeline...")
+    print("➡️ DespesaPipeline...")
     DespesaPipeline().run(municipio, ano)
+
+    print("\n✔ Pipelines finalizadas com sucesso!\n")
 
 if __name__ == "__main__":
     run_pipelines()
+
